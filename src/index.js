3cb4e3fb4b667ae4e5c15ee976644ea08d48b48c
Luis Sanchez <luis.sanchez.dev@gmail.com>
2023-08-06 01:01:33 -0500

    feature: add custom hooks useField

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import useField from './hooks/useField';
 
 const App = () => {
-  return <h1>Custom Hooks: user form</h1>
+  const userField = useField('text')
+  const emailField = useField('email')
+  const phoneField = useField('phone')
+
+
+  return (
+    <form>
+      <div>
+        <label>Name</label>
+        <input type={ userField.name } value={ userField.value } onChange={ userField.onChange } />
+      </div>
+      <div>
+        <label>Email</label>
+        <input type={ emailField.name } value={ emailField.value } onChange={ emailField.onChange } />
+      </div>
+      <div>
+        <label>Phone</label>
+        <input type={ phoneField.name } value={ phoneField.value } onChange={ phoneField.onChange } />
+      </div>
+      <button type='submit'>Send</button>
+    </form>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
